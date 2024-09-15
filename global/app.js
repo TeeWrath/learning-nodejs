b0f2999efc59a2c2c6108ac1ecdcca1612e73242
Subroto <subroto.2003@gmail.com>
2024-09-15 13:27:27 +0530

    initial commit

---
+++ global/app.js
@@ -0,0 +1,6 @@
+var randomVar = 'Subroto';
+
+// Below will result in undefined in the console
+// since the variable declared is not part of the
+// global object due to node module system
+global.console.log(global.randomVar)
