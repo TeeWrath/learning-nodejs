4fb60db742ea831cd155b3c20d94f75974a41276
Subroto <subroto.2003@gmail.com>
2024-09-19 01:24:21 +0530

    connected to Database

--- simple-crud-app/index.js
+++ simple-crud-app/index.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express();
-
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-})
 
 app.get('/', (req, res) => {
     res.send("Response sent from subbu's API");
+})
+
+mongoose.connect("mongodb+srv://subroto2003:eo0uD2JDuHu5BnDH@backenddb.sgeaf.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB").then(() => {
+    console.log("Connected DB");
+    app.listen(3000, () => {
+        console.log("Server is running on port 3000");
+    })
+}).catch(() => {
+    console.log("Connection failed");
 })
